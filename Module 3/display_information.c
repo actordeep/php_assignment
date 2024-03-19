c868827c68a3bd473c5d5b2df31ab5063ef80a7e
actordeep <115787025+actordeep@users.noreply.github.com>
2024-03-19 10:03:04 +0530

    modules

---
+++ Module 3/display_information.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char name[] = "Pradeep Yadav";
+    char birthDate[] = "October 15, 1999";
+    int age = 24;
+    char address[] = "Kotputli,Jaipur,Rajasthan,India";
+
+    printf("Name: %s\n", name);
+    printf("DOB: %s\n", birthDate);
+    printf("Age: %d\n", age);
+    printf("Address: %s\n", address);
+    
+    return 0;
+}
